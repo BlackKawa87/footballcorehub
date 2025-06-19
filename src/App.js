b666e70284ce7dde05b5672c5f88b3e49acdcb01
BlackKawa87 <140896510+BlackKawa87@users.noreply.github.com>
2025-06-19 22:20:29 +0100

    Add post-game analysis component with radar chart

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AnalisePosJogo from './components/AnalisePosJogo';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center p-4">
+      <AnalisePosJogo />
+    </div>
+  );
+}
+
+export default App;
