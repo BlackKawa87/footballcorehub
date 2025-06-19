f225b09f78f7714268b2818e1dbbb355d70473a2
BlackKawa87 <140896510+BlackKawa87@users.noreply.github.com>
2025-06-19 21:38:09 +0100

    Add basic React router structure

---
+++ src/components/DashboardJogador.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DashboardJogador = () => {
+  return (
+    <div>
+      <h1>Dashboard Jogador</h1>
+      <p>Painel do jogador.</p>
+    </div>
+  );
+};
+
+export default DashboardJogador;
