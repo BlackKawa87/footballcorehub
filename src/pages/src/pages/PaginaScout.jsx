77b9a39e5132e73268948a0433d947fb114846c6
BlackKawa87 <140896510+BlackKawa87@users.noreply.github.com>
2025-06-21 18:05:40 +0100

    Create PaginaScout.jsx

---
+++ src/pages/src/pages/PaginaScout.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const PaginaScout = () => (
+  <div className="p-8">
+    <h1 className="text-3xl font-bold mb-4">Página do Scout</h1>
+    <p>Essa é a tela dedicada aos scouts para buscar e analisar jogadores detalhadamente.</p>
+  </div>
+);
+
+export default PaginaScout;
