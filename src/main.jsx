3b49da4d665e048e511cea47cc901a5ad3367f43
BlackKawa87 <140896510+BlackKawa87@users.noreply.github.com>
2025-06-21 23:42:19 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import AppRoutes from './routes/AppRoutes.jsx';
-import './index.css';
+import App from './App.jsx';
+import './index.css'; // ← Isso importa o Tailwind
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
