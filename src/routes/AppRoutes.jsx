17b37b801d686300c2b98486cca1574cf803f022
BlackKawa87 <140896510+BlackKawa87@users.noreply.github.com>
2025-06-23 01:59:48 +0100

    Update AppRoutes.jsx

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,25 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import Home from '../pages/Home'; // <== Aqui está o novo
 
-import Home from '../pages/Home';
-// Aqui você pode importar as outras páginas conforme forem sendo criadas
-// import JogadorDashboard from '../pages/JogadorDashboard';
-// import ScoutDashboard from '../pages/ScoutDashboard';
-// import EmpresarioDashboard from '../pages/EmpresarioDashboard';
-// import ClubesDashboard from '../pages/ClubesDashboard';
-
-const AppRoutes = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/jogador" element={<JogadorDashboard />} />
-        <Route path="/scout" element={<ScoutDashboard />} />
-        <Route path="/empresario" element={<EmpresarioDashboard />} />
-        <Route path="/clubes" element={<ClubesDashboard />} /> */}
-      </Routes>
-    </Router>
-  );
-};
+const AppRoutes = () => (
+  <Routes>
+    <Route path="/" element={<Home />} />
+    {/* outras rotas */}
+  </Routes>
+);
 
 export default AppRoutes;
